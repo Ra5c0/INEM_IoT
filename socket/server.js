140cfc55f6b823506263834b2bd6d8b016ee372e
TheoDoucet <124595086+TheoDoucet@users.noreply.github.com>
2023-02-19 15:25:53 +0100

    Server v1
    Code for server
    Currently broadcasting

--- socket/server.js
+++ socket/server.js
@@ -4,6 +4,9 @@ var C = xbee_api.constants;
 //var storage = require("./storage")
 require('dotenv').config()
 
+var message = require("./message")
+
+//bouton_numero_0 = frame.digitalSamples.DIO0
 
 const SERIAL_PORT = process.env.SERIAL_PORT;
 
@@ -12,7 +15,7 @@ var xbeeAPI = new xbee_api.XBeeAPI({
 });
 
 let serialport = new SerialPort(SERIAL_PORT, {
-  baudRate: parseInt(process.env.SERIAL_BAUDRATE) || 9600,
+  baudRate: parseInt(process.env.SERIAL_BAUDRATE) ,
 }, function (err) {
   if (err) {
     return console.log('Error: ', err.message)
@@ -21,6 +24,8 @@ let serialport = new SerialPort(SERIAL_PORT, {
 
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
+
+//message.change_etat_led();
 
 serialport.on("open", function () {
   var frame_obj = { // AT Request to be sent
@@ -39,11 +44,64 @@ serialport.on("open", function () {
   };
   xbeeAPI.builder.write(frame_obj);
 
+  // --- Initialisation ------------
+  xbeeAPI.builder.write(message.change_master("D0", 0x03));
+  xbeeAPI.builder.write(message.change_master("D1", 0x03));
+  xbeeAPI.builder.write(message.change_master("D2", 0x03));
+  xbeeAPI.builder.write(message.change_master("D3", 0x03));
+
+
+  xbeeAPI.builder.write(message.change_master("P1", 0x04));
+  xbeeAPI.builder.write(message.change_master("D5", 0x04));
+  xbeeAPI.builder.write(message.change_master("D4", 0x04));
+  xbeeAPI.builder.write(message.change_master("D7", 0x04));
+
+
+  xbeeAPI.builder.write(message.change_master("IC", 0x0F));
+
+  affiche_simon()
+
 });
+
+async function affiche_simon() {
+  await message.sleep(1000)
+  tableau = message.var_tab()
+  longueur = message.var_tab_long()
+  console.log("Pre boucle -------------")
+
+  for (let index = 0; index < longueur; index++) {
+    const element = message.retourne_numero_pin_led(tableau[index]);
+
+    console.log("Pin " + element + " allumer")
+    xbeeAPI.builder.write(message.change_master(element, 0x05))
+    await message.sleep(300)
+    console.log("Pin " + element + " eteindre")
+    console.log(message.change_master(element, 0x04));
+    xbeeAPI.builder.write(message.change_master(element, 0x04))
+    xbeeAPI.builder.write(message.change_master(element, 0x04))
+    await message.sleep(60)
+
+    console.log("-->")
+  }
+
+  console.log("Post boucle")
+}
+
+async function allume_led(numero_led) {
+  const element = message.retourne_numero_pin_led(numero_led)
+  xbeeAPI.builder.write(message.change_master(element, 0x05))
+  await message.sleep(200)
+  xbeeAPI.builder.write(message.change_master(element, 0x04))
+}
 
 // All frames parsed by the XBee will be emitted here
 
 // storage.listSensors().then((sensors) => sensors.forEach((sensor) => console.log(sensor.data())))
+
+var state_buttons = -1;
+var return_info_after_pressed
+
+
 
 xbeeAPI.parser.on("data", function (frame) {
 
@@ -61,13 +119,34 @@ xbeeAPI.parser.on("data", function (frame) {
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
     // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
     console.log("NODE_IDENTIFICATION");
-    //storage.registerSensor(frame.remote64)
+    // storage.registerSensor(frame.remote64)
 
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
 
-    console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-    console.log(frame.analogSamples.AD0)
-    //storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
+    
+
+    state_buttons = message.receptionne_etat_bouton_retourne_nouv_presse(
+      frame.digitalSamples.DIO0,
+      frame.digitalSamples.DIO1,
+      frame.digitalSamples.DIO2,
+      frame.digitalSamples.DIO3)
+    if (message.simple_nex_button_pressed(state_buttons)) {
+      return_info_after_pressed = message.jeu_bouton_presse(state_buttons)
+      if (return_info_after_pressed == -1) {
+        allume_led(0)
+        allume_led(1)
+        allume_led(2)
+        allume_led(3)
+      }
+      else {
+        allume_led(state_buttons)
+        if (return_info_after_pressed==1) {
+          affiche_simon()
+        }
+      }
+    }
+
+    // storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
 
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
     console.log("REMOTE_COMMAND_RESPONSE")
